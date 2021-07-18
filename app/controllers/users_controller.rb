fb16b30c3f8b65da454d88bdcda3665526e76e09
Daita <daitamura2021@gmail.com>
2021-07-18 05:31:14 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user #redirect_to user_url(@user)を省略したもの
+
     else
       render 'new'
     end
